fa0496d56fb2eb6f6983cc4d662637333d106730
RUSSIAN-GOON <sammykioko754@gmail.com>
2025-10-02 21:45:14 +0300

    Add files via upload

---
+++ program if a student qualifies for exam.cpp
@@ -0,0 +1,29 @@
+*/*/
+NAME  :  KAMENE SAMMY KIOKO
+REG NO  : CT101/G/26524/25
+
+A PROGRAM TO DETERMINE WETHER A STUDENT QUALIFIES FOR AN EXAM 
+*/
+#include <stdio.h>
+
+int main() {
+    float attendance;
+    float averageMarks;
+
+    printf("Enter attendance percentage: ");
+    scanf("%f", &attendance);
+
+    printf("Enter average marks: ");
+    scanf("%f", &averageMarks);
+
+    if (attendance >= 75 && averageMarks >= 40) {
+        printf("Eligible\n");
+    } else {
+        printf("Not eligible\n");
+    }
+
+    return 0;
+}
+
+
+---
